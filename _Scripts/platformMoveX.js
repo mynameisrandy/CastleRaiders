ff9c5dff84bf407df24d914a0a582c34426c24a7
ferreiraj92 <ferreira.j92@gmail.com>
2016-02-13 03:11:55 -0500

    X axis platform move script

---
+++ _Scripts/platformMoveX.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+public var moveSpeed : float;
+private var direction : String = 'right';
+
+function Update () {
+	if(direction == 'right') {
+		transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+	}else {
+		transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+	}
+}
+
+function OnTriggerEnter(hit : Collider) {
+	if(hit.gameObject.tag == "waypointRight") {
+		//Debug.Log('right');
+		//transform.Rotate(0, 180, 0);
+		direction = 'left';
+	}
+
+	if(hit.gameObject.tag == "waypointLeft") {
+		//Debug.Log('left');
+		//transform.Rotate(0, 180, 0);
+		direction = 'right';
+	}
+}
